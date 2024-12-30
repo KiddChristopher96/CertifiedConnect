fbf1fa9eaac81b6c85bbba6caf3501a59c04f947
Christopher Kidd <budgetbuddyprohelp@gmail.com>
2024-12-29 22:14:09 -0500

    Initial Commit

---
+++ CertifiedConnect/CertifiedConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CertifiedConnectApp.swift
+//  CertifiedConnect
+//
+//  Created by Christopher Kidd on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CertifiedConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CertifiedConnectTests/CertifiedConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CertifiedConnectTests.swift
+//  CertifiedConnectTests
+//
+//  Created by Christopher Kidd on 12/29/24.
+//
+
+import Testing
+@testable import CertifiedConnect
+
+struct CertifiedConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
